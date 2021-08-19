065b86bdfdb09b2d6ca07c11b7aff023d3b600b8
men7627 <men1219@hanmail.net>
2021-08-20 00:47:25 +0900

    apollo client setting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ApolloClient, { gql } from 'apollo-boost'
+import ApolloProvider from 'apollo-boost';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' })
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const query = gql`
+  {
+    totalUsers
+    totalPhotos
+  }
+`
+
+console.log('cache', client.extract())
+client.query({query})
+  .then(({data}) => console.log('cache', client.extract()))
+  .catch(console.error)
+
+render(
+    <ApolloProvider client={client}>
+      <App />,
+    </ApolloProvider>, 
+    document.getElementById('root')
+)
